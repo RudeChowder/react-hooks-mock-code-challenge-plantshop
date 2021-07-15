a827da2d03bd457bff2fe1cb2d57a858917bf58a
Stephen Crane <scrane88@gmail.com>
2021-07-15 10:46:40 -0400

    Render list of plants

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  
+  const plantsURL = "http://localhost:6001/plants"
+
+  useEffect(() => {
+    fetch(plantsURL)
+      .then(resp => resp.json())
+      .then(data => setPlants(data))
+  }, [])
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
