88393e93a46ee52049d64cc5b0b46638f24ebe4d
Stephen Crane <scrane88@gmail.com>
2021-07-15 11:16:51 -0400

    Can mark a plant sold out

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard( { id, name, image, price } ) {
+  const [isInStock, setIsInStock] = useState(true)
+
+  const handleClick = () => {
+    setIsInStock(false)
+  }
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
